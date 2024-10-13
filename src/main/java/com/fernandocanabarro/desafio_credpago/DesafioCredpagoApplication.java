d03bded5cf90900e65c1b0f5971486fe2b05d474
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2024-10-13 12:12:23 -0300

    :tada: init: Projeto Criado

---
+++ src/main/java/com/fernandocanabarro/desafio_credpago/DesafioCredpagoApplication.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.desafio_credpago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCredpagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCredpagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandocanabarro/desafio_credpago/DesafioCredpagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.desafio_credpago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCredpagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
